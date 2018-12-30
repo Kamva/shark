44b4faa68c8c5a61ca93e8b4b58d42d0e78f50f6
Behzad Shabani <behzad.shabani@gmail.com>
2018-12-30 17:10:39 +0330

    Add exception reporter and renderer middlewares

---
+++ renderer.go
@@ -0,0 +1,42 @@
+package shark
+
+import (
+	"fmt"
+
+	"github.com/Kamva/pantopoda/http/api"
+	"github.com/Kamva/shark/exceptions"
+	"github.com/kataras/iris/context"
+)
+
+// ErrorRenderer renders errors and exception in json format
+func ErrorRenderer() context.Handler {
+	return func(context context.Context) {
+		defer renderError(context)
+
+		context.Next()
+	}
+}
+
+func renderError(context context.Context) {
+	response := api.NewResponse(context)
+	err := context.Values().Get("exception")
+
+	if err == nil {
+		return
+	}
+
+	if validation, ok := err.(exceptions.ValidationException); ok {
+		response.UnprocessableEntity(validation.GetCode(), api.Payload{
+			Message: validation.GetMessage(),
+			Data:    validation.GetErrors(),
+		})
+	} else if e, ok := err.(exceptions.GenericException); ok {
+		response.Response(e.GetCode(), e.GetStatus(), api.Payload{
+			Message: e.GetMessage(),
+		})
+	} else {
+		response.InternalServerError(exceptions.GENERAL, api.Payload{
+			Message: fmt.Sprint(err),
+		})
+	}
+}
