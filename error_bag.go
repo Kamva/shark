09f7db86268df4767731d0a4ea73f0312a1ec1e8
Behzad Shabani <behzad.shabani@gmail.com>
2019-04-23 22:46:17 +0430

    Update error bag

--- error_bag.go
+++ error_bag.go
@@ -1,5 +1,9 @@
 package shark
 
+// ErrorBagMapper is a function type that accept errors of a field, process
+// theme, and then return the processed errors.
+type ErrorBagMapper func([]string) []string
+
 // ErrorBag is a error container
 type ErrorBag struct {
 	errors map[string][]string
@@ -13,4 +17,14 @@ func (b ErrorBag) Append(key string, value string) {
 // GetErrors returns all errors of all keys
 func (b ErrorBag) GetErrors() map[string][]string {
 	return b.errors
+}
+
+// Map accepts a mapper function and run it in every errors slice and return
+// new error bag.
+func (b ErrorBag) Map(function ErrorBagMapper) ErrorBag {
+	for key, value := range b.errors {
+		b.errors[key] = function(value)
+	}
+
+	return b
 }
